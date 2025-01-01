259be732763170ceade0b4da1e56285324107d02
francisojeah <ojeahfrancis@gmail.com>
2025-01-02 00:11:53 +0100

    setup tailwind css for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    screens: {
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
-}
-
+};
