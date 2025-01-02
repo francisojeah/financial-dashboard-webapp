a2b44a75d2319079602e1d50521fa587b9e90083
francisojeah <ojeahfrancis@gmail.com>
2025-01-02 17:29:43 +0100

    update font to inter and add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,29 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        black: "#232323",
+        custom: {
+          success: "#8ADD21",
+          danger: "#DD2121",
+          primary: {
+            1: "#343C6A",
+            2: "#134493",
+            3: "#ACBCCA",
+          },
+          secondary: {
+            1: "#B5D2CB",
+            2: "#CDE1DC",
+            3: "#E6F0ED",
+          },
+          accent: {
+            1: "#40E0BA",
+            2: "#7FEAD1",
+            3: "#BFF4E8",
+          },
+        },
       },
     },
     screens: {
